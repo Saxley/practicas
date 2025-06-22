6a988c83d52413a978a9689e9d87d7bed0ca2f4e
foxcornio <luis-77.89@hotmail.com>
2025-06-22 13:55:41 -0600

    Se termina de comentar el ejercicio Point2

--- Point2d/Point2d.java
+++ Point2d/Point2d.java
@@ -16,44 +16,55 @@ public class Point2d{
   //Cabe mencionar que la clase static se ejecuta al momento de ejecutar la clase, por eso mismo para llamarlo hay que usar el nombre de la clase y no usar this, ya que this se refiere a un atributo del objeto y en este caso no es un atributo de un objeto es un atributo de clase.
     Point2d.numberOfPoints = 0;
   }
+  
   public Point2d(){// Este bloque de codigo crea por asi decirlo un punto nuevo en el origen.
     this(0.0,0.0); // Hacemos referencia a el objeto y le pasamos por parametro los datos solicitados Double x y Double y del conatructor. A esto se le llama delegacion de constructor.
   }
+  
   public Point2d(final Double x, final Double y){ // Este segundo constructor de la clase Point2d solicita 2 argumentos de tipo Double que seran constantes.
     this.x=x; // almacenamos el valor de la constante en la variable x del objeto.
     this.y=y; // almacenamos el valor de la constante y en la variable y del objeto.
     Point2d.numberOfPoints++; // aumentamos la variable numberOfPoints de la clase Point2d.
   }
+  
   public Double getX(){ // Devuelve el valor de la variable x del punto actual.
     return this.x;
   }
+  
   public Double getY(){// Devuelve el valor de la coordenada y del objeto actual.
     return this.y;
   }
+  
   public static Integer getNumberOfPoints(){ // Devuelve la cantidad de puntos creados al momento.
     return Point2d.numberOfPoints;
   }
+  
   public Double getRho(){ // Devuelve la distancia que existe del punto actual al origen.
     return sqrt(pow(x,2)+pow(y,2)); // calculamos la distancia con ayuda del teorema de pitagoras.
     //sqrt() es la funcion de java.lang.Math.* que asimila a una raiz cuadrada.
     //pow(valor,exponente) es la funcion que asimila a un numero con exponencia n.
   }
+  
   public Double getTheta(){ // Esta funcion retorna un valor de tipo Double y se dedica a calcular el angulo del punto actual respecto al origen.
     Double angrad = atan2(y,x); // atan2(y,x) es la funcion que calcula en radianes el angulo.
     return toDegrees(angrad); //toDegrees() es la funcion que hace la conversion de radianes a grados
   }
-  public Double getDistance(final Point2d p){
-    return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2));
+  
+  public Double getDistance(final Point2d p){ // Calcula la distancia que existe entre 2 puntos
+    return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2)); // utilizamos el teorema de pitagoras para calcular la distancia entre punto a y punto b.
   }
-  public void translate(final Double a, final Double b){
+  
+  public void translate(final Double a, final Double b){ // Sumamos la cantidad ingresada a los ejes x, y para trasladar el punto a una nueva coordenada
     x += a;
     y += b;
   }
-  public void scale(final Double factor){
+  
+  public void scale(final Double factor){ // Multiplicamos la cordenada actual x, y para scalar el punto
     x *= factor;
     y *= factor;
   }
-  public void rotate(final Point2d p, final Double angle){
+  
+  public void rotate(final Point2d p, final Double angle){ // rotamos el punto respecto al origen
     Double angrad = toRadians(angle);
     Double x1 = this.x - p.x;
     Double y1 = this.y - p.y;
